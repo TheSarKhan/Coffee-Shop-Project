3e27c886c322442c63f56b6d6a481a9a4a33ee4c
Serxan-Babayev-7599 <64852621+Serxan-Babayev-7599@users.noreply.github.com>
2024-06-07 21:18:21 +0400

    Project Created

---
+++ CoffeeShop/src/main/java/com/sarkhan/CoffeeShop/CoffeeShopApplication.java
@@ -0,0 +1,13 @@
+package com.sarkhan.CoffeeShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeShopApplication.class, args);
+	}
+
+}
---
+++ CoffeeShop/src/test/java/com/sarkhan/CoffeeShop/CoffeeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarkhan.CoffeeShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
