ab2ac19ce2ecd6bb170d62a3e465d535ff88140a
Serxan-Babayev-7599 <64852621+Serxan-Babayev-7599@users.noreply.github.com>
2024-06-07 21:29:08 +0400

    Connected DB

--- CoffeeShop/src/main/java/com/sarkhan/CoffeeShop/CoffeeShopApplication.java
+++ CoffeeShop/src/main/java/com/sarkhan/CoffeeShop/CoffeeShopApplication.java
@@ -8,6 +8,8 @@ public class CoffeeShopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoffeeShopApplication.class, args);
+		int alam=3;
+		System.out.println(alam);
 	}
 
 }
