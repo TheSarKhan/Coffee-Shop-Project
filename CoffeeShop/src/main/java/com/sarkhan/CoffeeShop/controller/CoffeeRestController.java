28032cb7e1989e31414ddde2bbeef880b2514e25
Serxan-Babayev-7599 <64852621+Serxan-Babayev-7599@users.noreply.github.com>
2024-06-07 23:40:12 +0400

    Model,JPA,RestController

---
+++ CoffeeShop/src/main/java/com/sarkhan/CoffeeShop/controller/CoffeeRestController.java
@@ -0,0 +1,26 @@
+package com.sarkhan.CoffeeShop.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sarkhan.CoffeeShop.jpa.coffeeJPA.CoffeeJPA;
+import com.sarkhan.CoffeeShop.model.Coffee;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping(path = "/coffees")
+public class CoffeeRestController {
+    @Autowired
+   private CoffeeJPA coffeeJPA;
+@GetMapping()
+public List<Coffee> findAll(){
+  return coffeeJPA.findAll();
+}
+
+}
---
+++ CoffeeShop/src/main/java/com/sarkhan/CoffeeShop/model/Coffee.java
@@ -0,0 +1,19 @@
+package com.sarkhan.CoffeeShop.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Coffee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String coffeeName;
+
+}
