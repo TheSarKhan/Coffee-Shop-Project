840f1bf62694c3bce6f79cb15cd85011df8ebfb6
Serxan-Babayev-7599 <64852621+Serxan-Babayev-7599@users.noreply.github.com>
2024-06-26 00:17:09 +0400

    Added card to home page and solved directory problem

--- CoffeeShop/src/main/java/com/sarkhan/CoffeeShop/controller/HomeController.java
+++ CoffeeShop/src/main/java/com/sarkhan/CoffeeShop/controller/HomeController.java
@@ -1,13 +1,25 @@
 package com.sarkhan.CoffeeShop.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping; 
 
+import com.sarkhan.CoffeeShop.jpa.coffeeJPA.CoffeeJPA;
+import com.sarkhan.CoffeeShop.model.Coffee;
+
+ 
 
 @Controller
 public class HomeController {
+    @Autowired
+    private CoffeeJPA coffeeJPA;
 @GetMapping(path="/home")
-public String showHomePage() {
+public String showHomePage(Model model) {
+    List<Coffee> coffees=coffeeJPA.findAll();
+ model.addAttribute("coffees",coffees);
     return "home";
 }
 @GetMapping(path="/error")
